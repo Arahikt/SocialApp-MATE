17a1087359c5fc2142dba0f1f4f0b3256d7d7ff6
Arahikt <Arahikt@gmail.com>
2021-11-26 21:30:09 -0800

    Registration form added.

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -26,14 +26,26 @@ export class AccountService {
         }
       })
     )
-  } 
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(map((user: User) => {
+      if (user) {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.currentUserSource.next(user)
+      }
+      // return user;
+    })
+    )
+  }
+
   setCurrentUser(user: User) {
     this.currentUserSource.next(user);
   }
 
   logout() {
     localStorage.removeItem('user');
-   this.currentUserSource.next(null);
+    this.currentUserSource.next(null);
   }
 }
 

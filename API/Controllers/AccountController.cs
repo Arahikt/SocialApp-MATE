e58a5102005908965f1c57f6cbbb0fb3bd5756c8
Arahikt <Arahikt@gmail.com>
2021-12-10 02:20:28 -0800

    Asp.net Identity added.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,65 +17,57 @@ namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
-        private readonly DataContext _context;
         private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
-        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
+        private readonly UserManager<AppUser> _userManager;
+        public SignInManager<AppUser> SignInManager { get; set; }
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
+         ITokenService tokenService, IMapper mapper)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
             _mapper = mapper;
             _tokenService = tokenService;
-            _context = context;
+
         }
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
 
-            if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
             var user = _mapper.Map<AppUser>(registerDto);
-            // password  hashing algoritm
-            //used using for disposing of hash class(HMACSHA512).
-            using var hmac = new HMACSHA512();
-                user.UserName = registerDto.Username.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-                user.PasswordSalt = hmac.Key;
-            
-            //tracking in entity framework
-            _context.Users.Add(user);
-            //calls database and saves in DB
-            await _context.SaveChangesAsync();
 
-            return new UserDto{
+            user.UserName = registerDto.Username.ToLower();
+            var result= await _userManager.CreateAsync(user,registerDto.Password);
+            if(!result.Succeeded) return BadRequest(result.Errors);
+            return new UserDto
+            {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user), 
-                KnownAs=user.KnownAs,
-                Gender= user.Gender
+                Token = _tokenService.CreateToken(user),
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<UserDto>>Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users
+            var user = await _userManager.Users
                 .Include(p => p.Photos)
-                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
             if (user == null) return Unauthorized("Invalid username");
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if(!result.Succeeded) return Unauthorized();
 
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i=0; i < computeHash.Length; i++)
-            {
-                if(computeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password.");
-            }
-             return new UserDto
+            return new UserDto
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs =user.KnownAs,
-               Gender= user.Gender 
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
@@ -82,7 +75,7 @@ namespace API.Controllers
 
         private async Task<bool> UserExists(string username)
         {
-            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+            return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
 namespace API.Extensions
@@ -12,18 +15,29 @@ namespace API.Extensions
     {
         public static IServiceCollection AddIdnetityServices(this IServiceCollection services, IConfiguration config)
         {
-services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
+            services.AddIdentityCore<AppUser>(opt =>
             {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                };
-            });
-        return services;
+                opt.Password.RequireNonAlphanumeric =false;
+                opt.Password.RequireUppercase=false;
+                
+            })
+            .AddRoles<AppRole>()
+            .AddRoleManager<RoleManager<AppRole>>()
+            .AddSignInManager<SignInManager<AppUser>>()
+            .AddRoleValidator<RoleValidator<AppRole>>()
+            .AddEntityFrameworkStores<DataContext>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                        .AddJwtBearer(options =>
+                        {
+                            options.TokenValidationParameters = new TokenValidationParameters
+                            {
+                                ValidateIssuerSigningKey = true,
+                                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                                ValidateIssuer = false,
+                                ValidateAudience = false,
+                            };
+                        });
+            return services;
         }
     }
 }
